eadb718fb3e4793d08dc29ccbf36e53334ed4ee0
Aolajre <118650527+Aolajre@users.noreply.github.com>
2023-06-27 02:11:22 -0700

    scripts

---
+++ you.sh
@@ -0,0 +1,13 @@
+# !bin/bash
+# This is a script used to get your github account working
+#git clone https://github.com/Aolajre/maven-web-application
+#cd kops-k8s
+git init
+git branch -M main
+git add .
+git commit -m "scripts"
+git remote add origin https://github.com/Aolajre/nodejs_app.git
+git push -uf origin main
+#Output from config 
+git status
+git remote -v
