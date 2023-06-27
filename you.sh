b6ab0b6e147cf6f3239c57cb4859a7237b31cdcb
Aolajre <118650527+Aolajre@users.noreply.github.com>
2023-06-27 02:13:59 -0700

    nodejs_commit

--- you.sh
+++ you.sh
@@ -2,12 +2,12 @@
 # This is a script used to get your github account working
 #git clone https://github.com/Aolajre/maven-web-application
 #cd kops-k8s
-git init
+#git init
 git branch -M main
 git add .
-git commit -m "nodejs"
+git commit -m "nodejs_commit"
 git remote add originn https://github.com/Aolajre/nodejs_app.git
-git push -uf origin main
+git push -uf originn main
 #Output from config 
 git status
 git remote -v
